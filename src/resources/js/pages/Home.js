25521b664e07ecdfc63f1198fcbb2df743586774
Ryusei <sidapple@gmail.com>
2022-05-30 01:07:40 +0900

    update APIから返ってきたスケール構成音表示

--- src/resources/js/pages/Home.js
+++ src/resources/js/pages/Home.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Card } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
+import axios from 'axios';
 import MainTable from '../components/MainTable';
+
 
 //スタイルの定義
 const useStyles = makeStyles((theme) => createStyles({
@@ -26,13 +28,63 @@ const keyList = ['C', 'C#-Db', 'D', 'D#-Eb', 'E', 'F', 'F#-Gb', 'G', 'G#-Ab', 'A
 const majorDegree = ['度数', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
 const functionList = ['機能', 'T', '代理SD', '代理T', 'SD', 'D', '代理T,SD', '代理D'];
 
-const scales = ['Major', 'Minor'];
-const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+// const scales = ['Major', 'Minor'];
+// let defaultNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+// let notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 const chords = ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'Bdim'];
 
 function Home() {
     //定義したスタイルを利用するための設定
     const classes = useStyles();
+
+    // function getScale(index){
+
+    // }
+
+    useEffect(() => {
+      getScalesData();
+    },[])
+    const [scales, setScales] = useState([]);
+    const getScalesData = () => {
+        axios
+          .get('/api/major?rootNumber=4')
+          .then(response => {
+              setScales(response.data);     //バックエンドから返ってきたデータでpostsを更新する
+              console.log(response.data); //取得データ確認用のconsole.log()
+          })
+          .catch(() => {
+              console.log('通信に失敗しました');
+          });
+    }
+      
+    let notes = [
+      scales.i,
+      scales.ii,
+      scales.iii,
+      scales.iv,
+      scales.v,
+      scales.vi,
+      scales.vii,
+    ];
+    // scales.map((scale) =>
+    //   notes.push({
+    //     i: scale.i,
+    //     ii: scale.ii,
+    //     iii: scale.iii,
+    //     iv: scale.iv,
+    //     v: scale.v,
+    //     vi: scale.vi,
+    //     vii: scale.vii,
+    //   })
+    // );
+
+    // const [notes, setNotes] = useState([]);
+    // setNotes(defaultNotes);
+
+    // then(response => {
+    //   setNotes(response.data); //バックエンドから返ってきたデータでpostsを更新する
+    //   console.log(response.data); //取得データ確認用のconsole.log()
+    // })
 
     return (
         <div className="container">
@@ -45,7 +97,7 @@ function Home() {
                             {/* キー選択 */}
                             <div>
                               {keyList.map((item, index) => (
-                                <Button color="secondary" variant="contained" href={'/' + item} className={classes.keyButton} key={index}>{item}</Button>
+                                <Button color="secondary" variant="contained" href={'/api/major?rootNumber=' + index} className={classes.keyButton} key={index}>{item}</Button>
                               ))}
                             </div>
                             {/* テーブル部分の定義 */}
